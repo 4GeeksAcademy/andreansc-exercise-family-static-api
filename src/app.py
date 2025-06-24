5124877b86d7c31394b4789021219f76f8f7972d
Andrea Nolasco <ant.andreansc@gmail.com>
2025-06-24 03:14:49 +0000

    Terminé el proyecto

--- src/app.py
+++ src/app.py
@@ -33,10 +33,53 @@ def sitemap():
 def handle_hello():
     # This is how you can use the Family datastructure by calling its methods
     members = jackson_family.get_all_members()
-    response_body = {"hello": "world",
-                     "family": members}
-    return jsonify(response_body), 200
+    return jsonify(members), 200
 
+
+@app.route('/members', methods=['POST'])
+def create_member():
+    body = request.get_json(silent=True)
+    if body is None:
+        return jsonify({'msg':'Debes enviar informacion al body'}), 400
+    #pedirle al usuario first_name, age, lucky_numbers
+    if 'first_name' not in body:
+        return jsonify({'msg':'El campo \'first_name\' es obligatorio'})
+    
+    if 'age' not in body:
+        return jsonify({'msg':'El campo \'age\' es obligatorio'})
+    
+    if 'lucky_numbers' not in body:
+        return jsonify({'msg':'El campo \'lucky_numbers\' es obligatorio'})
+    
+    new_member = {
+        'first_name':body['first_name'],
+        'last_name': jackson_family.last_name,
+        'age': body['age'],
+        'lucky_numbers': body['lucky_numbers']
+    }
+    if 'id' in body:
+        new_member['id'] = body['id']
+    else:
+        new_member['id'] = jackson_family._generate_id()
+    member = jackson_family.add_member(new_member)
+    
+    return jsonify(member), 200
+
+@app.route('/members/<int:id>', methods=['GET'])
+def get_member(id):
+    member = jackson_family.get_member(id)
+    if member:
+        return jsonify(member), 200
+    return jsonify({'msg': 'Miembro no encontrado'}), 404
+
+@app.route('/members/<int:id>', methods=['DELETE'])
+def delete_member(id):
+    deleted = jackson_family.delete_member(id)
+    if deleted:
+        return jsonify({'done': True }), 200
+    return jsonify({'msg': 'Miembro no encontrado'}), 404
+
+    return jsonify(members), 200
 
 
 # This only runs if `$ python src/app.py` is executed
